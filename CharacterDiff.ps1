5886caae3dbd7675209ad447143a857058934a6e
Vitali <vitali_sch@freenet.de>
2026-03-28 23:08:51 +0100

    Fixed bug in diff method

--- CharacterDiff.ps1
+++ CharacterDiff.ps1
@@ -9,45 +9,31 @@ param(
 
     [Alias("mode")]
     [Parameter(Mandatory=$true)]
-    [ValidateSet("line","char")]
+    [ValidateSet("line","lines","char")]
     [string] $strMode
 )
 
-
-function Get-LevenshteinDistance($s, $t) {
-
-    [int] $n = $s.Length
-    [int] $m = $t.Length
-
-    [int[,]] $d = New-Object 'int[,]' ($n + 1), ($m + 1)
-
-    for ([int] $i = 0; $i -le $n; $i++) { $d[$i,0] = $i }
-    for ([int] $j = 0; $j -le $m; $j++) { $d[0,$j] = $j }
-
-    for ($i = 1; $i -le $n; $i++) {
-        for ($j = 1; $j -le $m; $j++) {
-
-            [int] $cost = if ($s[$i-1] -eq $t[$j-1]) { 0 } else { 1 }
-
-            $d[$i,$j] = [Math]::Min(
-                [Math]::Min(
-                    $d[$i-1,$j] + 1,
-                    $d[$i,$j-1] + 1
-                ),
-                $d[$i-1,$j-1] + $cost
-            )
-        }
-    }
-
-    return $d[$n,$m]
-}
 
 function Get-CharDifference($strFileA, $strFileB) {
 
     [string] $textA = Get-Content $strFileA -Raw
     [string] $textB = Get-Content $strFileB -Raw
 
-    return Get-LevenshteinDistance $textA $textB
+    $charsA = [System.Collections.Generic.HashSet[string]]::new()
+    $charsB = [System.Collections.Generic.HashSet[string]]::new()
+
+    foreach ($char in $textA.ToCharArray()) {
+        [void] $charsA.Add([string] $char)
+    }
+
+    foreach ($char in $textB.ToCharArray()) {
+        [void] $charsB.Add([string] $char)
+    }
+
+    $distinctChars = [System.Collections.Generic.HashSet[string]]::new($charsA)
+    $distinctChars.SymmetricExceptWith($charsB)
+
+    return $distinctChars.Count
 }
 
 function Get-LineDifference($strFileA, $strFileB) {
@@ -55,20 +41,10 @@ function Get-LineDifference($strFileA, $strFileB) {
     [string[]] $linesA = Get-Content $strFileA
     [string[]] $linesB = Get-Content $strFileB
 
-    [int] $max = [Math]::Max($linesA.Count, $linesB.Count)
-    [int] $intDiff = 0
+    $distinctLines = [System.Collections.Generic.HashSet[string]]::new($linesA)
+    $distinctLines.SymmetricExceptWith($linesB)
 
-    for ([int]$i = 0; $i -lt $max; $i++) {
-
-        [string] $a = if ($i -lt $linesA.Count) { $linesA[$i] } else { "" }
-        [string] $b = if ($i -lt $linesB.Count) { $linesB[$i] } else { "" }
-
-        if ($a -ne $b) {
-            $intDiff++
-        }
-    }
-
-    return $intDiff
+    return $distinctLines.Count
 }
 
 if (! (Test-Path $strFileA)) {
@@ -84,6 +60,10 @@ if (! (Test-Path $strFileB)) {
 switch ($strMode) {
 
     "line" {
+        Get-LineDifference $strFileA $strFileB
+    }
+
+    "lines" {
         Get-LineDifference $strFileA $strFileB
     }
 
